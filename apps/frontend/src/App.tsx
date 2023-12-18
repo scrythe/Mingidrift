a7879605fdd65ae23681033b88c498e2ed18e815
Magomed Alimkhanov <scrythepvp@gmail.com>
2023-12-18 17:32:19 +0100

    feat: add solid frontend with basic setup

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import type { Component } from "solid-js";
+
+// import logo from "./logo.svg";
+// import styles from "./App.module.css";
+
+const App: Component = () => {
+  return <section></section>;
+};
+
+export default App;

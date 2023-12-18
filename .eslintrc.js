25f8941def5a80f6282fa820dc743d1182a9a185
Magomed Alimkhanov <scrythepvp@gmail.com>
2023-12-18 08:56:38 +0100

    feat: setup pnpm workplace and basic fastify app

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+};
---
+++ apps/backend/src/index.ts
@@ -0,0 +1,37 @@
+import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
+// import { Server, IncomingMessage, ServerResponse } from "http";
+
+const server: FastifyInstance = Fastify({});
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          pong: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+};
+
+server.get("/ping", opts, async (_request, _reply) => {
+  return { pong: "it worked!" };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+
+    // const address = server.server.address();
+    // const port = typeof address === "string" ? address : address?.port;
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();

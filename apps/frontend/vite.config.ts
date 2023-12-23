7c1adbb264e45984efda565dff39320595e9e4bc
Magomed Alimkhanov <scrythepvp@gmail.com>
2023-12-24 00:28:48 +0100

    build: add build for frontend

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
@@ -14,7 +14,10 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  preview: {
+    port: parseInt(process.env.PORT ?? "3000"),
+  },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });

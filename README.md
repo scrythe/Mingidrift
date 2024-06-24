af5c00e33688bbc67ff1aed69aec141dc25272d9
Magomed Alimkhanov <scrythepvp@gmail.com>
2024-06-24 22:06:07 +0200

    feat: restructure code and implement ecs later

--- README.md
+++
@@ -1,2 +0,0 @@
-# Mingidrift
-pixel 3d threejs javascript socket multiplayer car game with device orientation
--- test.js
+++
@@ -1,3 +0,0 @@
-this.id = (+new Date()).toString(16) + 
-        (Math.random() * 100000000 | 0).toString(16) +
-        ECS.Entity.prototype._count;
